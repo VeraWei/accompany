b3331e41f0c80a14f27d9674bb61bcfd1a0c9e64
Wei <vera@Weis-MacBook-Air.local>
2020-04-29 14:53:47 -0700

    rebuild

---
+++ build/precache-manifest.965e498f6ebf8d00a328058cdc6a0581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b071cd1959b47baaef3941585adbf319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3933fea026061121363a",
+    "url": "/static/css/main.cbd09d36.chunk.css"
+  },
+  {
+    "revision": "e5ec64a2577d3685ac4c",
+    "url": "/static/js/2.6befe6ee.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/static/js/2.6befe6ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3933fea026061121363a",
+    "url": "/static/js/main.dd2bda64.chunk.js"
+  },
+  {
+    "revision": "a880e67f9aa14aaa8818",
+    "url": "/static/js/runtime-main.fca3f9de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
