02392aa5866170026081967da8e2321247f7ec8d
Wei <vera@Weis-MacBook-Air.local>
2020-05-01 14:38:55 -0700

    change api host

--- src/util/url.tsx
+++ src/util/url.tsx
@@ -4,8 +4,8 @@ interface API {
 }
 export const HELPER_URL: API = {
     // FEELING: {},
-    JOKE: "http://localhost:3000/joke",
-    ADVICE: "http://localhost:3000/advice",
+    JOKE: "https://accompanybuddy.azurewebsites.net/joke",
+    ADVICE: "https://accompanybuddy.azurewebsites.net/advice",
     miku_v2: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/models/mmd/miku/miku_v2.pmd",
     wavefile_v2: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/models/mmd/vmds/wavefile_v2.vmd",
     wavefile_camera: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/models/mmd/vmds/wavefile_camera.vmd",
