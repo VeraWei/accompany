e640a9d975b1d04cd3917cf6593c2adaf230c346
Vera Wei <vera.wqm@gmail.com>
2020-05-01 15:08:25 -0700

    update

--- src/util/url.tsx
+++ src/util/url.tsx
@@ -5,7 +5,8 @@ interface API {
 export const HELPER_URL: API = {
     // FEELING: {},
     JOKE: "https://accompanybuddy.azurewebsites.net/joke",
-    ADVICE: "https://accompanybuddy.azurewebsites.net/advice",
+    // ADVICE: "https://accompanybuddy.azurewebsites.net/advice",
+    ADVICE: "http://localhost:3000/advice",
     miku_v2: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/models/mmd/miku/miku_v2.pmd",
     wavefile_v2: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/models/mmd/vmds/wavefile_v2.vmd",
     wavefile_camera: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/models/mmd/vmds/wavefile_camera.vmd",
