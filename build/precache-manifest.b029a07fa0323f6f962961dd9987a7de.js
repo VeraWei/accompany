3bdf2ec9956601e8090e249009318aa6b1c1229a
vera wei <vera.wqm@gmail.com>
2020-05-07 11:56:48 -0700

    rebuild

---
+++ build/precache-manifest.b029a07fa0323f6f962961dd9987a7de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0a581a969a795052b83dbd664666e7",
+    "url": "/self-isolation-fun-front/index.html"
+  },
+  {
+    "revision": "c869f2eac737cb351dc8",
+    "url": "/self-isolation-fun-front/static/css/2.e03f0d08.chunk.css"
+  },
+  {
+    "revision": "a4f16bd789f1792a1272",
+    "url": "/self-isolation-fun-front/static/css/main.c059b5b0.chunk.css"
+  },
+  {
+    "revision": "c869f2eac737cb351dc8",
+    "url": "/self-isolation-fun-front/static/js/2.1bb0be3e.chunk.js"
+  },
+  {
+    "revision": "7e0eb9e9c33b6ae29e606be9062cc91e",
+    "url": "/self-isolation-fun-front/static/js/2.1bb0be3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f16bd789f1792a1272",
+    "url": "/self-isolation-fun-front/static/js/main.f080f593.chunk.js"
+  },
+  {
+    "revision": "e679c6b12aad68a46be2",
+    "url": "/self-isolation-fun-front/static/js/runtime-main.95bd37bc.js"
+  }
+]);
