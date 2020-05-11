f67b84dac218bbf11ce165aae901ab2c5ef09c59
vera wei <vera.wqm@gmail.com>
2020-05-11 15:14:24 -0700

    build

---
+++ build/precache-manifest.bc9206d7fc81476be65cc59d97cd09c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b7e348ba73b22a37614a5cde7cc60c",
+    "url": "/self-isolation-fun-front/index.html"
+  },
+  {
+    "revision": "1a1193e12d69d4f31805",
+    "url": "/self-isolation-fun-front/static/css/2.e03f0d08.chunk.css"
+  },
+  {
+    "revision": "1b86ebaf3a88b0eb1a3a",
+    "url": "/self-isolation-fun-front/static/css/main.831b9fae.chunk.css"
+  },
+  {
+    "revision": "1a1193e12d69d4f31805",
+    "url": "/self-isolation-fun-front/static/js/2.be1f85e7.chunk.js"
+  },
+  {
+    "revision": "6c699240d6ba44b275d5a502e99fa513",
+    "url": "/self-isolation-fun-front/static/js/2.be1f85e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b86ebaf3a88b0eb1a3a",
+    "url": "/self-isolation-fun-front/static/js/main.a0dce043.chunk.js"
+  },
+  {
+    "revision": "e679c6b12aad68a46be2",
+    "url": "/self-isolation-fun-front/static/js/runtime-main.95bd37bc.js"
+  },
+  {
+    "revision": "8382dbf338f5867f2aaf0934959ba464",
+    "url": "/self-isolation-fun-front/static/media/logo.8382dbf3.png"
+  }
+]);
