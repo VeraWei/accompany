6a6ce0d58a8fcfa1110d00625f7afbbe422207fb
vera wei <vera.wqm@gmail.com>
2020-05-19 10:42:28 -0700

    build

---
+++ build/precache-manifest.9e822457fab36840912fb21421b7b73f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8341b915e30b63f28f9c9a488d058896",
+    "url": "/accompany/index.html"
+  },
+  {
+    "revision": "321ca15e980341bb1d9f",
+    "url": "/accompany/static/css/2.355c3bcb.chunk.css"
+  },
+  {
+    "revision": "5d01d41c8ca1afffcdd4",
+    "url": "/accompany/static/css/main.ea7a3013.chunk.css"
+  },
+  {
+    "revision": "321ca15e980341bb1d9f",
+    "url": "/accompany/static/js/2.5547b051.chunk.js"
+  },
+  {
+    "revision": "ab32a858d34fc91f24233b2e912f1d95",
+    "url": "/accompany/static/js/2.5547b051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d01d41c8ca1afffcdd4",
+    "url": "/accompany/static/js/main.7ab6543c.chunk.js"
+  },
+  {
+    "revision": "cb84ef11d81ec33c22c0",
+    "url": "/accompany/static/js/runtime-main.6aad57f1.js"
+  },
+  {
+    "revision": "8382dbf338f5867f2aaf0934959ba464",
+    "url": "/accompany/static/media/logo.8382dbf3.png"
+  }
+]);
