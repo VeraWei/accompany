9e6739d3e0957cc194a6643752189c468756ef02
vera wei <vera.wqm@gmail.com>
2020-05-19 10:32:09 -0700

    user center

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/accompany/precache-manifest.45b3ff8a19cb17e28ad9662fa1a894cc.js"
+  "/accompany/precache-manifest.39390fe244c36e815a493c00ade5b82e.js"
 );
 
 self.addEventListener('message', (event) => {
